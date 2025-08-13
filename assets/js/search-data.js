36662e0bc6ec818fb3ed813eae7aa6fb80f21e67
daviswelakuh <61803539+daviswelakuh@users.noreply.github.com>
2025-08-13 01:04:19 +0000

    Deploying to gh-pages from @ daviswelakuh/daviswelakuh.github.io@ff5fc3934ce2d7513b3b8f3cbd3a5a0c375d01ed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,7 +473,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%61%76%69%73.%77%65%6C%61%6B%75%68.%72%65%73%65%61%72%63%68@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -481,6 +481,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/davis-welakuh-10a5b928", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-9585-3406", "_blank");
         },
       },{
         id: 'social-rss',
@@ -494,7 +508,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=3ajskXsAAAAJ&hl", "_blank");
         },
       },{
         id: 'social-custom_social',
